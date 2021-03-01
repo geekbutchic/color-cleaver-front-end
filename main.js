c6ad469fbccab7523b19ed1ba4e113d9f9108799
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-02-28 21:20:42 -0500

    finalized all solutions

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ const isValidSecondary = require('./is-valid-secondary.js')
 
 
 // Your code here!
+console.clear();
+
+const input1 = getInput(1);
+const input2 = getInput(2);
+
+// console.log(input1);
+// console.log(input2);
+
+function getInput (n) {
+    return process.argv[n + 1];
+}
+
+if (input1 === undefined && input2 === undefined) {
+    console.log("Please enter at least one color!");
+} if (input === !undefined && input2 === undefined) {
+    console.log("That color deconstructed makes " + colorDeconstructor(input1) + "!");
+} else {
+    console.log("Those colors combined make " + colorCombinator(input1, input2) + '!');
+}
+
